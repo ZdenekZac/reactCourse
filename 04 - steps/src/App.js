34b57dbef17f027829d5b48a614936f9c9801f91
zac <zdenekdoskocil88@gmail.com>
2024-07-08 14:50:42 +0200

    8/7/2024

--- 04 - steps/src/App.js
+++ 04 - steps/src/App.js
@@ -1,49 +1,66 @@
 import { useState } from "react";
 
-const messages = ["laern react ", "get a job ", "invest money "];
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps /> */}
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "zac" });
   const [isOpen, setIsOpen] = useState(true);
+
+  // const [test, setTest] = useState({ name: "Jonas" });
 
   function handlePrevious() {
     if (step > 1) setStep((s) => s - 1);
-    if (step === 3) setTest({ name: "Zac" });
   }
+
   function handleNext() {
-    if (step < 3) setStep((s) => s + 1);
-    if (step === 2) setTest({ name: "fred" });
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
+
+    // BAD PRACTICE
+    // test.name = "Fred";
+    // setTest({ name: "Fred" });
   }
 
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen((s) => !s)}>
-        X
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
       </button>
+
       {isOpen && (
         <div className="steps">
           <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1.</div>
-            <div className={step >= 2 ? "active" : ""}>2.</div>
-            <div className={step >= 3 ? "active" : ""}>3.</div>
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
           <p className="message">
-            Step: {step} {messages[step - 1]}
-            {test.name}
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
           </p>
 
           <div className="buttons">
-            <button onClick={handlePrevious} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-              previous
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
+              Previous
             </button>
-            <button onClick={handleNext} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-              next
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
+              Next
             </button>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
