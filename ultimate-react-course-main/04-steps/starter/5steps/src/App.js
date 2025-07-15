60a9d1ab5a7f38be46791d2f03191e62e5988dbe
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2025-07-15 16:51:58 +0200

    at work steps

--- ultimate-react-course-main/04-steps/starter/5steps/src/App.js
+++ ultimate-react-course-main/04-steps/starter/5steps/src/App.js
@@ -1,36 +1,38 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
 export default function App() {
-  const [step, setStep] = useState(1);
+	const [number, setNumber] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
-  function handlePrev() {
-    if (step > 1) setStep(step - 1);
-  }
+	function handlePrev(){
+		if(number > 1) setNumber(number - 1)
+	}
 
-  function handleNext() {
-    if (step < 3) setStep(step + 1);
-  }
+	function handleNext(){
+		if(number < 3) setNumber(number + 1)
+	}
 
-  return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrev}>
-          prev
-        </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
-          next
-        </button>
-      </div>
-    </div>
-  );
+	return(
+		<>
+			<button className="close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
+			{isOpen &&
+				(
+			<div className="steps">
+				<div className="numbers">
+					<div className={number >= 1 ? "active" : ""}>1</div>
+					<div className={number >= 2 ? "active" : ""}>2</div>
+					<div className={number >= 3 ? "active" : ""}>3</div>
+				</div>
+				<p className="message">Step {number}: {messages[number - 1]}</p>
+				<div className="buttons">
+					<button onClick={handlePrev}>prev</button>
+					<button onClick={handleNext}>next</button>
+				</div>
+			</div>
+			)
+		}
+		</>
+	)
 }
--- ultimate-react-course-main/04-steps/starter/5steps/src/index.js
+++ ultimate-react-course-main/04-steps/starter/5steps/src/index.js
@@ -9,3 +9,35 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+	// const [step, setStep] = useState(1);
+	// const [isOpen, setIsOpen] = useState(true);
+
+	// function handlePrev(){
+	// 	if(step > 1) setStep(step - 1);
+	// }
+	// function handleNext(){
+	// 	if(step < 3) setStep(step + 1);
+	// }
+
+	// return(
+	// 	<>
+	// 		<span className="close" onClick={(()=> setIsOpen(!isOpen))}>&times;</span>
+	// 		{isOpen && 
+	// 	<div className="steps">
+	// 		<div className="numbers">
+
+	// 		<div className= "active">1</div>
+	// 		<div className={step > 1 ? "active" : ""}>2</div>
+	// 		<div className={step > 2 ? "active": ""}>3</div>
+	
+	// 		</div>
+	// 		<p className="message">Step {step}: {messages[step - 1]}</p>
+
+	// 		<div className="buttons">
+	// 		<button style={{ backgroundColor: "#7950f2", color:"#fff" }} onClick={handlePrev} ><span>prev</span></button>
+	// 		<button style={{ backgroundColor: "#7950f2", color:"#fff" }} onClick={handleNext}><span>next</span></button>
+	// 		</div>
+	// 	</div>
+	// 	}
+	// 	</>)
