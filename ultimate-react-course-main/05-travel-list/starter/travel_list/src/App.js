80ac4dad21dbbd8f8567faa7bd888c10d0430b79
zac <zdenekdoskocil88@gmail.com>
2025-07-17 22:17:16 +0200

    at homw

---
+++ ultimate-react-course-main/05-travel-list/starter/travel_list/src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away🌴</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>👉 What do you need for your trip? 👈</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      🎒
+      <em>you have X items on your list, and you already packed X 🎒</em>
+    </footer>
+  );
+}
