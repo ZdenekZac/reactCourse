4098fbf30b10ab5462eb2a11ca8db25bb9594cac
zac <zdenekdoskocil88@gmail.com>
2025-08-19 23:41:34 +0200

    items

--- ultimate-react-course-main/06B_trainingApp/starter/src/App.js
+++ ultimate-react-course-main/06B_trainingApp/starter/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 
 const dropdownMenuItems = [
-	{name: "hats", value:"hats"},
-	{name: "bicycles", value:"bicy"},
-	{name: "shoes", value:"shoes"},
-	{name: "eyeglasses", value:"eyes"},
-	{name: "hoodies", value:"hoodies"},
-	{name: "t-shirts", value:"tshirts"},
-]
+  { name: "hats", value: "hats" },
+  { name: "bicycles", value: "bicy" },
+  { name: "shoes", value: "shoes" },
+  { name: "eyeglasses", value: "eyes" },
+  { name: "hoodies", value: "hoodies" },
+  { name: "t-shirts", value: "tshirts" },
+];
 
 export default function App() {
   return (
@@ -22,33 +22,37 @@ export default function App() {
 function Menu() {
   return (
     <div className="menu">
-      <Dropdown onSelect={(val)=> console.log(val)
-	  }/>
-	  <img src="/Assets/shopping-cart.svg" alt="shopping_cart" />
+      <Dropdown onSelect={(val) => console.log(val)} />
+      <img src="/Assets/shopping-cart.svg" alt="shopping_cart" />
       <span className="span-menu">3</span>
     </div>
   );
 }
 
-function Dropdown({onSelect}){
-	return (
-		<div className="dropdown">
-			<img src="/Assets/list.svg" alt="menu" />
-			<ul className="dropdown-menu">
-				{dropdownMenuItems.map((item, i) =>
-					<li key={i} onClick={()=> onSelect(item.value)}>{item.name}</li>
-				)}
-			</ul>
-		</div>
-	)
+function Dropdown({ onSelect }) {
+  return (
+    <div className="dropdown">
+      <img src="/Assets/list.svg" alt="menu" />
+      <ul className="dropdown-menu">
+        {dropdownMenuItems.map((item, i) => (
+          <li key={i} onClick={() => onSelect(item.value)}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Main() {
-  return <main>
-	<div className="item">
-		<img src="/Assets/beanie.svg" alt="beanie"/>
-	</div>
-  </main>;
+  return (
+    <main>
+      <div className="item">
+        <h3>Beanie 1</h3>
+        <img src="/Assets/beanie.svg" alt="beanie" />
+      </div>
+    </main>
+  );
 }
 function Aside() {
   return <aside>Aside</aside>;
