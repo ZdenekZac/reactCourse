c77a7d995d54b7367cfc2651c7ec5eb48cec1988
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2025-12-15 17:42:35 +0100

    message

--- ultimate-react-course-main/10-react-quiz/starter/quiz/src/components/App.js
+++ ultimate-react-course-main/10-react-quiz/starter/quiz/src/components/App.js
@@ -1,8 +1,10 @@
 import Main from "./Main";
 import Header from "./Header";
 import Loader from "./Loader";
+import Progress from "./Progress";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 import Error from "./Error";
 import { useEffect, useReducer } from "react";
 
@@ -42,15 +44,21 @@ function reducer(state, action) {
         answer: action.payload,
         points: action.payload === question.correctOption ? state.points + question.points : state.points,
       };
+	case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1, answer: null
+      };
     default:
       throw new Error("action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) =>prev + cur.points, 0)
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -65,8 +73,16 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
+        {status === "ready" && (<StartScreen numQuestions={numQuestions} dispatch={dispatch} />)}
+        {status === "active" && (
+			<>
+			<Progress index={index} numQuestions={numQuestions} answer={answer} 
+				points={points} maxPossiblePoints={maxPossiblePoints}/>
+			<Question question={questions[index]} dispatch={dispatch} answer={answer} />
+			<NextButton dispatch={dispatch} answer={answer}/>		
+			</>
+		)
+	}
       </Main>
     </div>
   );
