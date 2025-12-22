1583e866e039329eada8460fd01da6cd30452dcb
zac <zdenekdoskocil88@gmail.com>
2025-12-22 22:34:14 +0100

    lll

--- ultimate-react-course-main/10-react-quiz/starter/quiz/src/components/NextButton.js
+++ ultimate-react-course-main/10-react-quiz/starter/quiz/src/components/NextButton.js
@@ -1,10 +1,10 @@
-function NextButton({dispatch, answer}){
-	if (answer === null) return null;
+function NextButton({ dispatch, answer }) {
+  if (answer === null) return null;
 
-	return (
-    <button className="btn btn-ui" onClick={()=>
-		  dispatch({type: "nextQuestion"})}>
-        Next
-    </button>)
+  return (
+    <button className="btn btn-ui" onClick={() => dispatch({ type: "nextQuestion" })}>
+      Next
+    </button>
+  );
 }
-export default NextButton
+export default NextButton;
