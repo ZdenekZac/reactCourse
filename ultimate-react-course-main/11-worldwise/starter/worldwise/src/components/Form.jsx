7f06cd303b4ccf4255fca67af7f751b17ff82ab7
zac <zdenekdoskocil88@gmail.com>
2026-02-04 22:12:42 +0100

    merge

--- ultimate-react-course-main/11-worldwise/starter/worldwise/src/components/Form.jsx
+++ ultimate-react-course-main/11-worldwise/starter/worldwise/src/components/Form.jsx
@@ -1,13 +1,15 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useEffect, useState } from "react";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
 import Message from "./Message";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 import Spinner from "./Spinner";
+import { useCities } from "../contexts/CitiesContext";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -20,56 +22,79 @@ export function convertToEmoji(countryCode) {
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
-    const [lat, lng] = useUrlPosition();
-    const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
+  const [lat, lng] = useUrlPosition();
+  const { createCity } = useCities();
+
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
   const [emoji, setEmoji] = useState("");
-  const [geocodingError, setGeocodingError] = useState("")
+  const [geocodingError, setGeocodingError] = useState("");
 
+  useEffect(
+    function () {
+      if (!lat && !lng) return;
 
-  useEffect(function(){
-    async function fetchCityData(){
-      try{
+      async function fetchCityData() {
+        try {
           setIsLoadingGeocoding(true);
           setGeocodingError("");
 
           const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
           const data = await res.json();
 
-          if(!data.countryCode) throw new Error("No city over here, click somewhere else")
+          if (!data.countryCode) throw new Error("No city over here, click somewhere else");
 
           setCityName(data.city || data.locality || "");
           setCountry(data.countryName);
           setEmoji(convertToEmoji(data.countryCode));
-          console.log(data);
-          
-      }catch(err){
+        } catch (err) {
           setGeocodingError(err.message);
-      }finally {
-        setIsLoadingGeocoding(false)
+        } finally {
+          setIsLoadingGeocoding(false);
+        }
       }
-    }
-    fetchCityData();
-  },[lat, lng]);
+      fetchCityData();
+    },
+    [lat, lng],
+  );
 
-  if(isLoadingGeocoding) return <Spinner />
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  if(geocodingError) return <Message message={geocodingError}/>;
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+    createCity(newCity);
+  }
+
+  if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng) return <Message message="start by clicking somewhere on the map" />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input id="cityName" onChange={(e) => setCityName(e.target.value)} value={cityName} />
-         <span className={styles.flag}>{emoji}</span> 
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input id="date" onChange={(e) => setDate(e.target.value)} value={date} />
+
+        <DatePicker id="date" onChange={(date) => setDate(date)} selected={date} dateFormat="dd/MM/yyyy" />
       </div>
 
       <div className={styles.row}>
