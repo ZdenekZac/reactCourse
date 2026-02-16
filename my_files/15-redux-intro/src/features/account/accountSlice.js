df169581c7ca0fed01d6509ebe443392a98bfc6f
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-02-16 17:45:53 +0100

    redux toolkit and thunk

--- my_files/15-redux-intro/src/features/account/accountSlice.js
+++ my_files/15-redux-intro/src/features/account/accountSlice.js
@@ -1,10 +1,73 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action){
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action){
+      state.balance -= action.payload;
+    },
+
+    requestLoan:{ 
+      prepare(amount, purpose){
+        return {
+          payload: {amount, purpose},
+        };
+      },
+
+      reducer(state, action){
+        if(state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+
+    payLoan(state, action){
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+
+    convertingCurrency(state){
+      state.isLoading = true;
+    }
+  },
+});
+
+console.log(accountSlice);
+
+export const {withdraw, requestLoan, payLoan} = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    // API call
+    const res = await fetch(`https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`);
+    const data = await res.json();
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export default accountSlice.reducer;
+
+/*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -19,9 +82,9 @@ export default function accountReducer(state = initialStateAccount, action) {
         ...state,
         balance: state.balance - action.payload,
       };
-
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
+      
+      case "account/requestLoan":
+        if (state.loan > 0) return state;
       // LATER
       return {
         ...state,
@@ -68,3 +131,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan(amount) {
   return { type: "account/payLoan" };
 }
+*/
---
+++ my_files/15-redux-intro/src/store-v2.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+import accountReducer from "./features/account/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+configureStore({
+  reducer:{
+    account: accountReducer,
+    customer: customerReducer
+  }
+})
+export default store;
