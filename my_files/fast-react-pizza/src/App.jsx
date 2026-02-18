0fad178019549f81579d247651d8515d8a793b06
zac <zdenekdoskocil88@gmail.com>
2026-02-18 21:53:41 +0100

    routes

--- my_files/fast-react-pizza/src/App.jsx
+++ my_files/fast-react-pizza/src/App.jsx
@@ -1,7 +1,7 @@
-import Cart from "./features/cart/Cart"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Cart from "./features/cart/Cart";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
@@ -10,17 +10,17 @@ const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
-      {path: "/",element: <Home />},
-      {path: "/menu", element: <Menu />},
-      {path: "/cart", element: <Cart />},
-      {path: "/order/new", element: <CreateOrder />},
-      {path: "/order/:orderId", element: <Order />},
-    ]
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
   },
-])
+]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
