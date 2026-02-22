f56562339d1ff6001f82694450ce1b67c79f0806
zac <zdenekdoskocil88@gmail.com>
2026-02-22 11:40:04 +0100

    tailwind

--- my_files/fast-react-pizza/prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-// prettier.config.js
-
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  singleQuote: true,
-};
