6dc98d7423e6055fc3b684bb0cd43c751826305b
zac <zdenekdoskocil88@gmail.com>
2026-02-23 21:19:44 +0100

    tailwind-config extends etc

--- my_files/fast-react-pizza/src/ui/AppLayout.jsx
+++ my_files/fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
 import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   const navigation = useNavigation();
@@ -10,6 +10,7 @@ function AppLayout() {
   return (
     <div className='grid h-screen grid-rows-[auto_1fr_auto]'>
       {isLoading && <Loader />}
+
       <Header />
       <div className='overflow-scroll'>
         <main className='mx-auto max-w-3xl overflow-scroll'>
