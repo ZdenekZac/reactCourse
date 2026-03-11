e9bf1be996bf6b9f72ce6369a854f3aac09d3b88
zac <zdenekdoskocil88@gmail.com>
2026-03-11 23:19:28 +0100

    createOrder useActionData hook

--- my_files/refresh-pizza/src/features/order/CreateOrder.jsx
+++ my_files/refresh-pizza/src/features/order/CreateOrder.jsx
@@ -1,9 +1,25 @@
-import { Form, redirect, useNavigation } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 export default function CreateOrder() {
+  const [withPriority, setWithPriority] = useState(false);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
+
+  // tady jsem skoncil 11.03. 23:19
+  const formErrors = useActionData();
 
   return (
     <div>
