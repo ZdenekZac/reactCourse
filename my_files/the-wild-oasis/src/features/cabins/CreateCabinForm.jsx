70040e7f0195d04ef9e882d93f331ac1654291d3
zac <zdenekdoskocil88@gmail.com>
2026-04-07 21:46:14 +0200

    FormRow fixed

--- my_files/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ my_files/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,3 @@
-import styled from 'styled-components';
-
 import Input from '../../ui/Input';
 import Form from '../../ui/Form';
 import Button from '../../ui/Button';
@@ -9,42 +7,7 @@ import { useForm } from 'react-hook-form';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { createCabin } from '../../services/apiCabins';
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import FormRow from '../../ui/FormRow';
 
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
@@ -71,20 +34,22 @@ function CreateCabinForm() {
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
-      <FormRow label="Cabin name" error={errors?.name?.message}>
+      <FormRow label='Cabin name' error={errors?.name?.message}>
         <Input
-          type="text"
-          id="name"
+          type='text'
+          id='name'
+          disabled={isCreating}
           {...register('name', {
             required: 'This field is required!!',
           })}
         />
       </FormRow>
 
-      <FormRow label="Max capacity" error={errors?.maxCapacity?.message}>
+      <FormRow label='Max capacity' error={errors?.maxCapacity?.message}>
         <Input
-          type="number"
-          id="maxCapacity"
+          type='number'
+          id='maxCapacity'
+          disabled={isCreating}
           {...register('maxCapacity', {
             required: 'This field is required!!',
             min: {
@@ -95,10 +60,11 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
         <Input
-          type="number"
-          id="regularPrice"
+          type='number'
+          id='regularPrice'
+          disabled={isCreating}
           {...register('regularPrice', {
             required: 'This field is required!!',
             min: {
@@ -109,42 +75,40 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow label="Discount" error={errors?.discount?.message}>
+      <FormRow label='Discount' error={errors?.discount?.message}>
         <Input
-          type="number"
-          id="discount"
+          type='number'
+          id='discount'
+          disabled={isCreating}
           defaultValue={0}
           {...register('discount', {
             required: 'This field is required!!',
+            valueAsNumber: true,
             validate: (value) =>
-              value <= getValues().regularPrice ||
-              'Discount should be less than regular price',
+              value <= Number(getValues().regularPrice) || 'Discount should be less than regular price',
           })}
         />
       </FormRow>
 
-      <FormRow
-        label="Description for website"
-        error={errors?.description?.message}
-      >
+      <FormRow label='Description for website' disabled={isCreating} error={errors?.description?.message}>
         <Textarea
-          type="number"
-          id="description"
-          defaultValue=""
+          type='number'
+          id='description'
+          defaultValue=''
+          disabled={isCreating}
           {...register('description', {
             required: 'This field is required!!',
           })}
         />
       </FormRow>
 
-      <FormRow label="Cabin photo">
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+      <FormRow label='Cabin photo'>
+        <FileInput id='image' accept='image/*' />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation='secondary' type='reset'>
           Cancel
         </Button>
         <Button disabled={isCreating}>Add cabin</Button>
