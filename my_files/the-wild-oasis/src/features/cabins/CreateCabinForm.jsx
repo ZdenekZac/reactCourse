15947c1a78a2b4d1b919910e101dd9a3c5518023
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-04-08 17:17:18 +0200

    22:21 error

--- my_files/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ my_files/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,22 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
 import Input from '../../ui/Input';
 import Form from '../../ui/Form';
 import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
-import { useForm } from 'react-hook-form';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
-import { createCabin } from '../../services/apiCabins';
 import FormRow from '../../ui/FormRow';
 
-function CreateCabinForm() {
-  const { register, handleSubmit, reset, getValues, formState } = useForm();
+import { createEditCabin } from '../../services/apiCabins';
+import { useForm } from 'react-hook-form';
+
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { id: editId, ...editValues } = cabinToEdit;
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
   const { errors } = formState;
-  console.log(errors);
+
   const queryClient = useQueryClient();
 
-  const { mutate, isPending: isCreating } = useMutation({
-    mutationFn: createCabin,
+  const { mutate: createCabin, isPending: isCreating } = useMutation({
+    mutationFn: createEditCabin,
     onSuccess: () => {
       toast.success('New cabin successfully created!');
       queryClient.invalidateQueries({ queryKey: ['cabins'] });
@@ -25,8 +32,24 @@ function CreateCabinForm() {
     onError: (err) => toast.error(err.message),
   });
 
+  const { mutate: editCabin, isPending: isEditing } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success('Cabin successfully edited!');
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const isWorking = isCreating || isEditing;
+
   function onSubmit(data) {
-    mutate(data);
+    const image = typeof data.image === 'string' ? data.image : data.image[0];
+
+    if (isEditSession)
+      editCabin({ newCabinData: { ...data, image }, id: editId });
+    else createCabin({ ...data, image: image });
   }
 
   function onError(errors) {
@@ -34,22 +57,22 @@ function CreateCabinForm() {
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
-      <FormRow label='Cabin name' error={errors?.name?.message}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
-          type='text'
-          id='name'
-          disabled={isCreating}
+          type="text"
+          id="name"
+          disabled={isWorking}
           {...register('name', {
             required: 'This field is required!!',
           })}
         />
       </FormRow>
 
-      <FormRow label='Max capacity' error={errors?.maxCapacity?.message}>
+      <FormRow label="Max capacity" error={errors?.maxCapacity?.message}>
         <Input
-          type='number'
-          id='maxCapacity'
-          disabled={isCreating}
+          type="number"
+          id="maxCapacity"
+          disabled={isWorking}
           {...register('maxCapacity', {
             required: 'This field is required!!',
             min: {
@@ -60,11 +83,11 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
         <Input
-          type='number'
-          id='regularPrice'
-          disabled={isCreating}
+          type="number"
+          id="regularPrice"
+          disabled={isWorking}
           {...register('regularPrice', {
             required: 'This field is required!!',
             min: {
@@ -75,43 +98,56 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow label='Discount' error={errors?.discount?.message}>
+      <FormRow label="Discount" error={errors?.discount?.message}>
         <Input
-          type='number'
-          id='discount'
-          disabled={isCreating}
+          type="number"
+          id="discount"
+          disabled={isWorking}
           defaultValue={0}
           {...register('discount', {
             required: 'This field is required!!',
             valueAsNumber: true,
             validate: (value) =>
-              value <= Number(getValues().regularPrice) || 'Discount should be less than regular price',
+              value <= Number(getValues().regularPrice) ||
+              'Discount should be less than regular price',
           })}
         />
       </FormRow>
 
-      <FormRow label='Description for website' disabled={isCreating} error={errors?.description?.message}>
+      <FormRow
+        label="Description for website"
+        disabled={isWorking}
+        error={errors?.description?.message}
+      >
         <Textarea
-          type='number'
-          id='description'
-          defaultValue=''
-          disabled={isCreating}
+          type="number"
+          id="description"
+          defaultValue=""
+          disabled={isWorking}
           {...register('description', {
             required: 'This field is required!!',
           })}
         />
       </FormRow>
 
-      <FormRow label='Cabin photo'>
-        <FileInput id='image' accept='image/*' />
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register('image', {
+            required: isEditSession ? false : 'This field is required!!',
+          })}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button disabled={isCreating}>Add cabin</Button>
+        <Button disabled={isWorking}>
+          {isEditSession ? 'Edit cabin' : 'Create new cabin'}
+        </Button>
       </FormRow>
     </Form>
   );
--- my_files/the-wild-oasis/src/services/apiCabins.js
+++ my_files/the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -20,13 +20,42 @@ export async function getCabins() {
 //   }
 // }
 
-export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from('cabins').insert([newCabin]);
-  //.select();
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabase);
+
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace('/', '');
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create/Edit cabin
+  let query = supabase.from('cabins');
+
+  // A) CREATE
+  if (!id) query.insert([{ ...newCabin, image: imagePath }]);
+
+  // B) EDIT
+  if (id) query.update({ ...newCabin, image: imagePath }).eq('id', id);
+
+  const { data, error } = await query.select().single();
 
   if (error) {
     console.error(error);
-    throw new Error('cabins could not be created :(');
+    throw new Error('cabin could not be created :(');
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error(
+      'cabin image could not be uploaded and the cabin was not created :('
+    );
   }
 
   return data;
