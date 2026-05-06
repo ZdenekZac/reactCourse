4974cea1aa00d60b3307cd53af511ff814d738f3
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-05-06 16:57:39 +0200

    .....

--- my_files/the-wild-ride/src/features/vans/CreateVanForm.jsx
+++ my_files/the-wild-ride/src/features/vans/CreateVanForm.jsx
@@ -5,12 +5,17 @@ import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
 import FormRow from '../../ui/FormRow';
 
-import { useCreateVans } from './useCreateVans';
 import { useForm } from 'react-hook-form';
-import useEditVan from './useEditVan';
+import { useCreateVans } from './useCreateVans';
+import { useEditVan } from './useEditVan';
 
-function CreateVanForm() {
-  return <div></div>;
+function CreateVanForm({ vanToEdit = {} }) {
+  const { id: editId, ...editValues } = vanToEdit;
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
 }
 
 export default CreateVanForm;
