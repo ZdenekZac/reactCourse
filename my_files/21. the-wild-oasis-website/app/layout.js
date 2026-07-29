7cd4c8f637083982b416ed294a4f5094feef789e
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-07-29 16:41:11 +0200

    about page

--- my_files/21. the-wild-oasis-website/app/layout.js
+++ my_files/21. the-wild-oasis-website/app/layout.js
@@ -4,6 +4,7 @@ import Navigation from '@/app//_components/Navigation';
 import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -20,20 +21,23 @@ export const metadata = {
   },
   description:
     'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased 
+        bg-primary-950 text-primary-100 min-h-screen flex flex-col
+        relative`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
