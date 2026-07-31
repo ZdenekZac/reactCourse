92be9e90d9f9172a533c0c5b31634a7df18c334e
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-07-31 15:02:50 +0200

    sidenavigation

--- my_files/21. the-wild-oasis-website/app/layout.js
+++ my_files/21. the-wild-oasis-website/app/layout.js
@@ -11,8 +11,6 @@ const josefin = Josefin_Sans({
   display: 'swap',
 });
 
-console.log(josefin);
-
 export const metadata = {
   // title: 'The Wild Oasis',
   title: {
@@ -35,8 +33,8 @@ export default function RootLayout({ children }) {
         relative`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
