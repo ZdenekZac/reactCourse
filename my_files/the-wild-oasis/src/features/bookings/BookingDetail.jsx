e9f0dad2443931e4845b1b15f22cca5b67f8eb7a
Zdeněk Doskočil <zdoskocil@qexcz.cz>
2026-08-26 13:21:36 +0200

    added uploader to PROD

--- my_files/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ my_files/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -34,7 +34,7 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
-  if (!booking) return <Empty resourceName='booking' />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
   const statusToTagName = {
@@ -45,9 +45,9 @@ function BookingDetail() {
 
   return (
     <>
-      <Row type='horizontal'>
+      <Row type="horizontal">
         <HeadingGroup>
-          <Heading as='h1'>Booking #{bookingId}</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
@@ -56,22 +56,30 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
-        {status === 'unconfirmed' && <Button onClick={() => navigate(`/checkin/${bookingId}`)}>Check in</Button>}
+        {status === 'unconfirmed' && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
 
         {status === 'checked-in' && (
-          <Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
             Check out
           </Button>
         )}
 
         <Modal>
-          <Modal.Open opens='delete'>
-            <Button $variation='danger'>Delete booking</Button>
+          <Modal.Open opens="delete">
+            <Button $variation="danger">Delete booking</Button>
           </Modal.Open>
 
-          <Modal.Window name='delete'>
+          <Modal.Window name="delete">
             <ConfirmDelete
-              resourceName='bookings'
+              resourceName="bookings"
               disabled={isDeleting}
               onConfirm={() =>
                 deleteBooking(bookingId, {
@@ -82,7 +90,7 @@ function BookingDetail() {
           </Modal.Window>
         </Modal>
 
-        <Button variation='secondary' onClick={moveBack}>
+        <Button $variation="secondary" onClick={moveBack}>
           Back
         </Button>
       </ButtonGroup>
